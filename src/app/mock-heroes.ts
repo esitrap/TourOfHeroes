e7486369d2b80c05ee2eae49b5e17862e1e230be
Ali <esitrap@gmail.com>
2019-07-12 10:46:56 +0430

    Copy From Desktop In Office

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+
+export const HEROES: Hero[] = [
+{id: 11 , name: 'Haj Rasoul'},
+{id: 12 , name:'Haj Agha' },
+{id:13 , name: 'Bagher'},
+{id:14 , name: 'Haj Amin'},
+{id:15 , name: 'Haj Masoud'},
+{id: 16 , name: 'PirPatal'},
+{id: 17 , name: 'Ebi'},
+{id: 18 , name: 'abi'},
+{id: 19 , name: 'amoghli'},
+{id: 20, name: 'Reza Parhizkar'}
+];
+
