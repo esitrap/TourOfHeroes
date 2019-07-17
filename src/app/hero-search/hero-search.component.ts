80d00767587f5af9fc0bb456c6126c116ed5c7bd
esitrap <esitrap@gmail.com>
2019-07-17 16:08:52 +0430

    Before Css Change

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { debounceTime, switchMap, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+heroes$: Observable<Hero[]>
+private searchTerm = new Subject<string>();
+  constructor(private heroService:HeroService) { }
+search(term:string):void{
+  this.searchTerm.next(term);
+}
+  ngOnInit() : void{
+  this.heroes$ = this.searchTerm.pipe (
+    debounceTime(300),
+    distinctUntilChanged(),
+    switchMap((term: string)=>this.heroService.searchHeroes(term)),
+  );
   }
 
 }
